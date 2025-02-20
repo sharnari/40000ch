77b85c86c9183c37960e4dd893796205ffa17292
sharnari <armgubin.sergey@gmail.com>
2025-02-21 02:23:07 +0500

    refactoring conponent

--- src/components/FormLogin/FormLogin.tsx
+++ src/components/FormLogin/FormLogin.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import { useForm, Controller, SubmitHandler } from 'react-hook-form'
-import { LockOutlined, UserOutlined } from '@ant-design/icons'
-import { Input, Button, Divider } from 'antd'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { UserOutlined } from '@ant-design/icons'
+import { Button, Divider } from 'antd'
 import InputCustomAnt from '../ui/InputCustomAnt'
 
 import styles from './formLogin.module.scss'
@@ -46,49 +46,15 @@ function FormLogin() {
           placeholder="Ваше имя при регистрации"
           status={errors.username ? 'error' : undefined}
         />
-        <div className={styles.inputContainer} style={{ marginBottom: '16px' }}>
-          <Controller
-            name="username"
-            control={control}
-            rules={{ required: 'Имя обязательно для заполнения' }}
-            render={({ field }) => (
-              <Input
-                className={styles.inputField}
-                size="large"
-                {...field}
-                prefix={<UserOutlined />}
-                placeholder="Ваше имя при регистрации"
-                status={errors.username ? 'error' : undefined}
-              />
-            )}
-          />
-          {errors.username && <p style={{ color: 'red', fontSize: '12px', margin: 0 }}>{errors.username.message}</p>}
-        </div>
-        <div className={styles.inputContainer} style={{ marginBottom: '16px' }}>
-          <Controller
-            name="password"
-            control={control}
-            rules={{
-              required: 'Поле обязательно для заполнения',
-              minLength: {
-                value: 6,
-                message: 'Пароль должен содержать не менее 6 символов',
-              },
-            }}
-            render={({ field }) => (
-              <Input
-                className={styles.inputField}
-                size="large"
-                {...field}
-                prefix={<LockOutlined />}
-                placeholder="Введите пароль"
-                type="password"
-                status={errors.password ? 'error' : undefined}
-              />
-            )}
-          />
-          {errors.password && <p style={{ color: 'red', fontSize: '12px', margin: 0 }}>{errors.password.message}</p>}
-        </div>
+        <InputCustomAnt
+          name="password"
+          control={control}
+          rules={{ required: 'Поле обязательно для заполнения' }}
+          size="large"
+          prefix={<UserOutlined />}
+          placeholder="Введите пароль"
+          status={errors.username ? 'error' : undefined}
+        />
         <Button style={{ borderRadius: '20px' }} size="large" type="primary" htmlType="submit" loading={loading}>
           Войти
         </Button>
